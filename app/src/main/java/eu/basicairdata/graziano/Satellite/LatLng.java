9e5ebe877d69214644e710f3c92076c2f661fb39
aditi <aditisawant007@gmail.com>
2020-06-05 03:00:22 +0530

    Initial commit

---
+++ app/src/main/java/eu/basicairdata/graziano/Satellite/LatLng.java
@@ -0,0 +1,7 @@
+
+package eu.basicairdata.graziano.Satellite;
+
+class LatLng {
+    double Latitude;
+    double Longitude;
+}
---
+++ app/src/main/java/eu/basicairdata/graziano/Satellite/PhysicalData.java
@@ -0,0 +1,7 @@
+
+package eu.basicairdata.graziano.Satellite;
+
+class PhysicalData {
+    String Value;       //The string of the Numerical value of the Physical Quantity
+    String UM;          //The string of the Unit of Measurement
+}
